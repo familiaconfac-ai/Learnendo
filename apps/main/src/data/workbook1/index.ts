122c53004dc81fdd9746e4362ab0d64d41f13e07
Márcio <familiaconfac@gmail.com>
2026-06-12 20:07:56 -0300

    update: ajustes e melhorias no projeto

--- apps/main/src/data/workbook1/index.ts
+++ apps/main/src/data/workbook1/index.ts
@@ -3,10 +3,12 @@ import { lesson2 } from './lesson2';
 import { lesson3 } from './lesson3';
 import { lesson4 } from './lesson4';
 import { lesson5 } from './lesson5';
+import { lesson6 } from './lesson6';
+import { lesson7 } from './lesson7';
 // Add more lessons as needed
 
 export const workbook1 = {
   id: 'wb1',
   title: 'Workbook 1',
-  lessons: [lesson1, lesson2, lesson3, lesson4, lesson5]
+  lessons: [lesson1, lesson2, lesson3, lesson4, lesson5, lesson6, lesson7],
 };
